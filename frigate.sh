9c87ccc613d52fe72e0283705b5bb648ad8a9222
mercuryin <45206429+mercuryin@users.noreply.github.com>
2024-06-12 14:17:26 +0100

    Update frigate.sh

--- frigate.sh
+++ frigate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/mercuryin/Proxmox-Frigate-Script/main/build.func)
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Authors: tteck (tteckster)
 # License: MIT
@@ -54,85 +54,11 @@ function default_settings() {
 }
 
 function update_script() {
-  if [[ ! -f /etc/systemd/system/frigate.service ]]; then 
-    msg_error "No ${APP} Installation Found!"
-    exit 1
-  fi
+  if [[ ! -f /etc/systemd/system/frigate.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
   msg_error "There is currently no update path available."
-  exit 1
+  exit  
 }
 
-# Definir la función para la instalación
-function install_script() {
-  pve_check
-  shell_check
-  root_check
-  arch_check
-  ssh_check
-
-  if systemctl is-active -q ping-instances.service; then
-    systemctl -q stop ping-instances.service
-  fi
-  NEXTID=$(pvesh get /cluster/nextid)
-  timezone=$(cat /etc/timezone)
-  header_info
-  if (whiptail --backtitle "Proxmox VE Helper Scripts" --title "SETTINGS" --yesno "Use Default Settings?" --no-button Advanced 10 58); then
-    header_info
-    echo -e "${BL}Using Default Settings${CL}"
-    default_settings
-  else
-    header_info
-    echo -e "${RD}Using Advanced Settings${CL}"
-    advanced_settings
-  fi
-}
-
-# Función para iniciar la instalación
-function start() {
-  if command -v pveversion >/dev/null 2>&1; then
-    if ! (whiptail --backtitle "Proxmox VE Helper Scripts" --title "${APP} LXC" --yesno "This will create a New ${APP} LXC. Proceed?" 10 58); then
-      clear
-      echo -e "⚠  User exited script \n"
-      exit 1
-    fi
-    SPINNER_PID=""
-    install_script
-  fi
-
-  if ! command -v pveversion >/dev/null 2>&1; then
-    if ! (whiptail --backtitle "Proxmox VE Helper Scripts" --title "${APP} LXC UPDATE" --yesno "Support/Update functions for ${APP} LXC. Proceed?" 10 58); then
-      clear
-      echo -e "⚠  User exited script \n"
-      exit 1
-    fi
-    SPINNER_PID=""
-    update_script
-  fi
-}
-
-# Función para construir el contenedor
-function build_container() {
-  # Código para construir el contenedor
-  msg_info "Building container"
-  # Aquí debe estar el código que construye el contenedor
-  msg_ok "Container built"
-}
-
-# Función para la descripción del contenedor
-function description() {
-  IP=$(pct exec "$CTID" ip a s dev eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
-  pct set "$CTID" -description "<div align='center'><a href='https://Helper-Scripts.com' target='_blank' rel='noopener noreferrer'><img src='https://raw.githubusercontent.com/tteck/Proxmox/main/misc/images/logo-81x112.png'/></a>
-
-  # ${APP} LXC
-
-  <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/&#x2615;-Buy me a coffee-blue' /></a>
-  </div>"
-  if [[ -f /etc/systemd/system/ping-instances.service ]]; then
-    systemctl start ping-instances.service
-  fi
-}
-
-# Iniciar el script
 start
 build_container
 description
